203819e9158ee06eac62b4ae118213da4e2e1c4a
Derek Butler <derekbutler@Dereks-MacBook-Pro.local>
2019-11-19 10:34:23 -0700

    WhatDoYouCallACowWithNoLegs

---
+++ src/Components/Derek.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Derek extends React.Component{
+    render(){
+        return(
+            <div>
+                SUP DUDES
+                Don't forget to NPM start so you can see me in your browser
+            </div>
+        )
+    }
+}
+export default Derek;
