8fd2e8ca64dc2ddd471bfb01044ce51d8980b3d2
Ryan Morgan <rrgy80@gmail.com>
2019-11-19 10:51:33 -0700

    ryan was here

---
+++ src/Components/Ryan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Ryan extends Component {
+
+    render() {
+        return (
+            <div>
+                R2-D2 goes beep boop
+            </div>
+        )
+    }
+}
+export default Ryan
