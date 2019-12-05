1fff1812aa74f3256d558cd569aecec743dc2454
Than <than.jarvis@gmail.com>
2019-12-05 10:17:05 -0700

    pre push commit, enpoint tests done

--- src/components/testsBro/functions.js
+++ src/components/testsBro/functions.js
@@ -9,12 +9,34 @@ export async function createPost() {
 }
 
 export async function getEmployeeRating(){
-    // const endval = ''
     const variable = await axios.post('http://localhost:3030/api/employeeRatingsRetrieval', { questionId: 0, receiverId: 1 })
     .then(res => {
         return res.data
     })
     .catch(err => err)
     return variable
-    //you have to make it match this format witht the axuis request for it to return what you wnat
+}
+
+export async function testingGetTeamEndpoint(){
+    const team = await axios.get('http://localhost:3030/api/team/1')
+    .then(res => {
+        return res.data
+    })
+    .catch(err => err)
+    return team
+}
+
+export async function testingGetPosts(){
+    const posts = await axios('http://localhost:3030/api/posts/1')
+    .then(res => res.data)
+    .catch(err => err)
+    return posts
+}
+
+export async function testGetPoll(){
+    const poll = await axios('http://localhost:3030/api/poll')
+    .then(res => res.data)
+    .catch(err => err)
+    return poll
+
 }
